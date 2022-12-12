1469a977c219ba4dacb8efa0856ad03ead1fde7f
IncredibleYoung <yellowgg2@gmail.com>
2022-12-12 14:17:33 +0000

    add korean to make

--- build-scripts/build-emscripten.sh
+++ build-scripts/build-emscripten.sh
@@ -1,4 +1,4 @@
 emsdk install 3.1.26
 emsdk activate 3.1.26
 
-make -j8 NATIVE=emscripten BACKTRACE=0 TILES=1 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js
+make -j8 NATIVE=emscripten LANGUAGES="ko en" BACKTRACE=0 TILES=1 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js
